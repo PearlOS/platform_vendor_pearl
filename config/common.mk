a9956746fd06f0e52f5666b31a2df1c3c7315efc
Rashed Abdel-Tawab <rashed@linux.com>
2018-09-06 08:48:26 -0700

    Dynamically add custom APNs
    For Example new devices need the new Sprint APNs, so add a new board flag for
    them to set that will make the build system add the new APNs to
    the APN config XML. If the flag is not set, continue to use the
    old APNs.
    To add or replace APNs, the custom_apns.py script has been
    added. If CUSTOM_APNS_FILE is defined, custom_apns.py
    is run and generates a new version of apns-conf.xml.
    
    Change-Id: I7ff12a4342de2a7663b2b66fd627244214a8dc71
    Signed-off-by: Genkzsz11 <genkzsz11@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -29,22 +29,22 @@ PRODUCT_PRODUCT_PROPERTIES += \
     net.tethering.noprovisioning=true
 
 PRODUCT_COPY_FILES += \
-    vendor/du/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
-    vendor/du/prebuilt/common/bin/sysinit:system/bin/sysinit
+    vendor/pearl/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
+    vendor/pearl/prebuilt/common/bin/sysinit:system/bin/sysinit
 
 # Init files
 PRODUCT_COPY_FILES += \
-    vendor/du/prebuilt/common/etc/init.local.rc:system/etc/init/dirtyunicorns.rc
+    vendor/pearl/prebuilt/common/etc/init.local.rc:system/etc/init/dirtyunicorns.rc
 
 # Don't export PS1 in /system/etc/mkshrc.
 PRODUCT_COPY_FILES += \
-    vendor/du/prebuilt/common/etc/mkshrc:system/etc/mkshrc
+    vendor/pearl/prebuilt/common/etc/mkshrc:system/etc/mkshrc
 
 # Backup tool
 PRODUCT_COPY_FILES += \
     vendor/extras/build/tools/backuptool.sh:install/bin/backuptool.sh \
     vendor/extras/build/tools/backuptool.functions:install/bin/backuptool.functions \
-    vendor/extras/build/tools/50-du.sh:system/addon.d/50-du.sh
+    vendor/extras/build/tools/50-pearl.sh:system/addon.d/50-pearl.sh
 
 # Weather client
 #PRODUCT_COPY_FILES += \
@@ -52,13 +52,13 @@ PRODUCT_COPY_FILES += \
 #    vendor/du/prebuilt/common/etc/default-permissions/org.pixelexperience.weather.client.xml:system/etc/default-permissions/org.pixelexperience.weather.client.xml
 
 # Packages
-include vendor/du/config/packages.mk
+include vendor/pearl/config/packages.mk
 
 # Branding
-include vendor/du/config/branding.mk
+include vendor/pearl/config/branding.mk
 
 # Themes
 include vendor/themes/common.mk
 
 # Overlays
-#PRODUCT_PACKAGE_OVERLAYS += vendor/du/overlay/common
+#PRODUCT_PACKAGE_OVERLAYS += vendor/pearl/overlay/common
