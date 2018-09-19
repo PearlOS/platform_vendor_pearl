f10a38b2e87a8450b0edaf9510e3fe6e9b3aa31b
Rashed Abdel-Tawab <rashed@linux.com>
2018-09-18 21:53:24 -0700

    Dynamically generate kernel headers using lineage generator
    Add a soong vendor plugin for kernel config variables so we can use
    these in go.
    
    Change-Id: Id31f2be8fcc5aba2d965dbe815edaaf1d28279c6
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- config/BoardConfigPEARL.mk
+++ config/BoardConfigPEARL.mk
@@ -3,3 +3,4 @@ include vendor/du/config/BoardConfigKernel.mk
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/pearl/config/BoardConfigQcom.mk
 endif
+include vendor/pearl/config/BoardConfigSoong.mk
