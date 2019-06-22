ab87c9f905af158cd9d70540165f030b76878c63
Ethan Chen <intervigil@gmail.com>
2019-06-22 14:12:40 -0700

    build: Separate platform macros from QCOM platform definition
    * The macros used in the file formerly called "qcom_utils.mk" are
      actually generic and not QCOM-specific. Move the QCOM_BOARD definitions
      to a QCOM-specific location, and name the remaning macros something more
      generic.
    
    Change-Id: I6747bc9201e56b456caf251122057d7dbc233f1c
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- build/core/config.mk
+++ build/core/config.mk
@@ -18,10 +18,7 @@ ifneq ($(TARGET_BUILD_VARIANT),user)
 SELINUX_IGNORE_NEVERALLOWS := true
 endif
 
-# Rules for MTK targets
-include $(TOPDIR)vendor/pearl/build/core/mtk_target.mk
-
 # Rules for QCOM targets
 include $(TOPDIR)vendor/pearl/build/core/qcom_target.mk
 
-BUILD_RRO_SYSTEM_PACKAGE := $(TOP)/vendor/du/build/core/system_rro.mk
+BUILD_RRO_SYSTEM_PACKAGE := $(TOP)/vendor/pearl/build/core/system_rro.mk
--- build/core/definitions.mk
+++ build/core/definitions.mk
@@ -1,9 +1,12 @@
 #
-# DU-specific macros
+# PEARL-specific macros
 #
 define uniq
 $(if $1,$(firstword $1) $(call uniq,$(filter-out $(firstword $1),$1)))
 endef
 
-# Include QCOM macros
-include vendor/pearl/build/core/qcom_utils.mk
+# Include board/platform macros
+include vendor/pearl/build/core/utils.mk
+
+# Include vendor platform definitions
+include vendor/pearl/build/core/vendor/*.mk
