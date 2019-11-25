Corvus-Q/vendor_corvus

vendor/build: Include MTK/QCOM rules
Change-Id: I22ea0998ab585b6434d3c14055e9c06a29f53846

build/core/config.mk
@@ -1,0 +1,20 @@
+
+# Copyright (C) 2018 AOSiP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Rules for MTK targets
+include $(TOPDIR)vendor/du/build/core/mtk_target.mk
+
+# Rules for QCOM targets
+include $(TOPDIR)vendor/du/build/core/qcom_target.mk

PearlOS/platform_vendor_pearl

build/core/config.mk
@@ -1,0 +1,20 @@
+
+# Copyright (C) 2018 AOSiP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Rules for MTK targets
+include $(TOPDIR)vendor/pearl/build/core/mtk_target.mk
+
+# Rules for QCOM targets
+include $(TOPDIR)vendor/pearl/build/core/qcom_target.mk