NusantaraProject-ROM/android_vendor_nusantara

vendor: Include makefile with art definitions
Change-Id: Ied15be62e315db3061dfb216ea6c7f0fdb43aa22
Signed-off-by: mydongistiny <jaysonedson@gmail.com>
Signed-off-by: blinoff82 <blinov.in@gmail.com>

config/common.mk
@@ -73,5 +73,8 @@ include vendor/du/config/branding.mk
 # Themes
 include vendor/themes/common.mk
 
+# Art
+include vendor/du/config/art.mk
+
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/du/overlay/common

PearlOS/platform_vendor_pearl

config/common.mk
@@ -85,6 +85,9 @@ include vendor/pearl/config/branding.mk
 # Themes
 include vendor/themes/common.mk
 
+# Art
+include vendor/pearl/config/art.mk
+
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/pearl/overlay/common
 DEVICE_PACKAGE_OVERLAYS += vendor/pearl/overlay/common