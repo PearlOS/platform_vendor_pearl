Corvus-Q/vendor_corvus

envsetup: add breakfast
Change-Id: I9ac8a4025d9ccfd5bee31a5fe9d1b490171bcbe7

build/envsetup.sh
@@ -16,3 +16,35 @@ function mka() {
             ;;
     esac
 }
+
+function breakfast()
+{
+    target=$1
+    DU_DEVICES_ONLY="true"
+    unset LUNCH_MENU_CHOICES
+    add_lunch_combo full-eng
+    for f in `/bin/ls vendor/du/vendorsetup.sh 2> /dev/null`
+        do
+            echo "including $f"
+            . $f
+        done
+    unset f
+
+    if [ $# -eq 0 ]; then
+        # No arguments, so let's have the full menu
+        echo "Nothing to eat for breakfast?"
+        lunch
+    else
+        echo "z$target" | grep -q "-"
+        if [ $? -eq 0 ]; then
+            # A buildtype was specified, assume a full device name
+            lunch $target
+        else
+            # This is probably just the DU model name
+            lunch durex_$target-userdebug
+        fi
+    fi
+    return $?
+}
+
+alias bib=breakfast

PearlOS/platform_vendor_pearl

build/envsetup.sh
@@ -16,3 +16,35 @@ function mka() {
             ;;
     esac
 }
+
+function breakfast()
+{
+    target=$1
+    PEARL_DEVICES_ONLY="true"
+    unset LUNCH_MENU_CHOICES
+    add_lunch_combo full-eng
+    for f in `/bin/ls vendor/pearl/vendorsetup.sh 2> /dev/null`
+        do
+            echo "including $f"
+            . $f
+        done
+    unset f
+
+    if [ $# -eq 0 ]; then
+        # No arguments, so let's have the full menu
+        echo "Nothing to eat for breakfast?"
+        lunch
+    else
+        echo "z$target" | grep -q "-"
+        if [ $? -eq 0 ]; then
+            # A buildtype was specified, assume a full device name
+            lunch $target
+        else
+            # This is probably just the Pearl model name
+            lunch pearl_$target-userdebug
+        fi
+    fi
+    return $?
+}
+
+alias bib=breakfast