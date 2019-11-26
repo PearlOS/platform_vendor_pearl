828ea0664e9b0d8d2547758f23b523e6713dbfa4
Sagarrokade006 <sagarrokade006@gmail.com>
2019-11-26 06:13:41 +0000

    Revert "vendor: Add special conditions for devices with prebuilt display hal"
    This reverts commit 70721861cfe79c04b16076f0b6d3cd00172e6ace.

--- config/BoardConfigPEARL.mk
+++ config/BoardConfigPEARL.mk
@@ -3,9 +3,4 @@ include vendor/pearl/config/BoardConfigKernel.mk
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/pearl/config/BoardConfigQcom.mk
 endif
-
-ifneq ($(FORCE_QCOM_DISPLAY_HAL_VARIANT),)
-include vendor/pearl/config/BoardConfigQcomDisplayOverride.mk
-endif
-
 include vendor/pearl/config/BoardConfigSoong.mk
