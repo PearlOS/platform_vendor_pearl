43db7d270c319c814afc51016ec3220ca1ceb157
mikeNG <mikeNG@users.noreply.github.com>
2019-11-26 06:30:49 +0000

    config: Replace 'system' with '$(TARGET_COPY_OUT_SYSTEM)'

--- config/common.mk
+++ config/common.mk
@@ -29,20 +29,20 @@ PRODUCT_PRODUCT_PROPERTIES += \
     net.tethering.noprovisioning=true
 
 PRODUCT_COPY_FILES += \
-    vendor/pearl/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
-    vendor/pearl/prebuilt/common/bin/sysinit:system/bin/sysinit
+    vendor/pearl/prebuilt/common/etc/init.d/00banner:$(TARGET_COPY_OUT_SYSTEM)/etc/init.d/00banner \
+    vendor/pearl/prebuilt/common/bin/sysinit:$(TARGET_COPY_OUT_SYSTEM)/bin/sysinit
 
 # Copy all custom init rc files
-$(foreach f,$(wildcard vendor/du/prebuilt/common/etc/init/*.rc),\
-    $(eval PRODUCT_COPY_FILES += $(f):system/etc/init/$(notdir $f)))
+$(foreach f,$(wildcard vendor/pearl/prebuilt/common/etc/init/*.rc),\
+    $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_SYSTEM)/etc/init/$(notdir $f)))
 
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+    frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.sip.voip.xml
 
 # Enable wireless Xbox 360 controller support
 PRODUCT_COPY_FILES += \
-    frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_0719.kl
+    frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/Vendor_045e_Product_0719.kl
 
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
@@ -56,13 +56,13 @@ PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 PRODUCT_COPY_FILES += \
     vendor/pearl/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
     vendor/pearl/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/pearl/prebuilt/common/bin/50-pearl.sh:system/addon.d/50-pearl.sh \
-    vendor/pearl/prebuilt/common/bin/blacklist:system/addon.d/blacklist
+    vendor/pearl/prebuilt/common/bin/50-pearl.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-pearl.sh \
+    vendor/pearl/prebuilt/common/bin/blacklist:$(TARGET_COPY_OUT_SYSTEM)/addon.d/blacklist
 
 # Weather client
 #PRODUCT_COPY_FILES += \
-#    vendor/pearl/prebuilt/common/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml \
-#    vendor/pearl/prebuilt/common/etc/default-permissions/org.pixelexperience.weather.client.xml:system/etc/default-permissions/org.pixelexperience.weather.client.xml
+#    vendor/pearl/prebuilt/common/etc/permissions/org.pixelexperience.weather.client.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.pixelexperience.weather.client.xml \
+#    vendor/pearl/prebuilt/common/etc/default-permissions/org.pixelexperience.weather.client.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/default-permissions/org.pixelexperience.weather.client.xml
 
 # Filesystems tools
 PRODUCT_PACKAGES += \
