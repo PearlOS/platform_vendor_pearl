17a497912f32a6edb61f46a9a44689ef5b19cbcf
Sagarrokade006 <sagarrokade006@gmail.com>
2019-11-26 07:11:24 +0000

    Cleanup Unused Stuff

--- config/common.mk
+++ config/common.mk
@@ -1,23 +1,27 @@
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
+PRODUCT_BRAND ?= PEARL-OS
+
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
-PRODUCT_PRODUCT_PROPERTIES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=android-google
 else
-PRODUCT_PRODUCT_PROPERTIES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
 endif
 
+ifneq ($(TARGET_BUILD_VARIANT),user)
+# Thank you, please drive thru!
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.dun.override=0
+endif
+
 # General additions
-PRODUCT_PRODUCT_PROPERTIES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     keyguard.no_require_sim=true \
-    dalvik.vm.debug.alloc=0 \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
-    ro.error.receiver.system.apps=com.google.android.gms \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dataroaming=false \
-    ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
     ro.com.android.dateformat=MM-dd-yyyy \
     persist.debug.wfd.enable=1 \
     persist.sys.wfd.virtual=0 \
@@ -25,7 +29,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.build.selinux=1
 
 # Allow tethering without provisioning app
-PRODUCT_PRODUCT_PROPERTIES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     net.tethering.noprovisioning=true
 
 PRODUCT_COPY_FILES += \
@@ -35,10 +39,6 @@ PRODUCT_COPY_FILES += \
 # Copy all custom init rc files
 $(foreach f,$(wildcard vendor/pearl/prebuilt/common/etc/init/*.rc),\
     $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_SYSTEM)/etc/init/$(notdir $f)))
-
-# Enable SIP+VoIP on all targets
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.sip.voip.xml
 
 # Enable wireless Xbox 360 controller support
 PRODUCT_COPY_FILES += \
@@ -51,19 +51,14 @@ PRODUCT_COPY_FILES += \
     vendor/pearl/prebuilt/common/bin/50-pearl.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-pearl.sh \
     vendor/pearl/prebuilt/common/bin/blacklist:$(TARGET_COPY_OUT_SYSTEM)/addon.d/blacklist
 
-# Weather client
-#PRODUCT_COPY_FILES += \
-#    vendor/pearl/prebuilt/common/etc/permissions/org.pixelexperience.weather.client.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.pixelexperience.weather.client.xml \
-#    vendor/pearl/prebuilt/common/etc/default-permissions/org.pixelexperience.weather.client.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/default-permissions/org.pixelexperience.weather.client.xml
-
 # Recorder permissions
 PRODUCT_COPY_FILES += \
     vendor/pearl/prebuilt/common/etc/permissions/privapp-permissions-recorder.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-recorder.xml
 
-# Power whitelist
 PRODUCT_COPY_FILES += \
     vendor/pearl/prebuilt/common/etc/permissions/pearl-power-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/pearl-power-whitelist.xml \
-    vendor/pearl/prebuilt/common/etc/permissions/privapp-permissions-pearl.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-pearl.xml \
+    vendor/pearl/prebuilt/common/etc/permissions/privapp-permissions-pearl--system.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-pearl-system.xml \
+    vendor/pearl/prebuilt/common/etc/permissions/privapp-permissions-pearl-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-pearl-product.xml \
     vendor/pearl/prebuilt/common/etc/permissions/pearl-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/pearl-hiddenapi-package-whitelist.xml
 
 # Filesystems tools
