PixelExperience/vendor_aosp

vendor: Add special conditions for devices with prebuilt display hal
* FORCE_QCOM_DISPLAY_HAL_VARIANT

Change-Id: I0045b6b58059c8e39724059e35a40cf8e9fc0039

config/BoardConfigQcom.mk
@@ -89,20 +89,7 @@ ifneq ($(OVERRIDE_QCOM_HARDWARE_VARIANT),)
 QCOM_HARDWARE_VARIANT := $(OVERRIDE_QCOM_HARDWARE_VARIANT)
 endif
 
-# Required for frameworks/native
-ifeq ($(QCOM_HARDWARE_VARIANT),msm8996)
-    TARGET_USES_QCOM_UM_FAMILY := true
-    TARGET_USES_QCOM_UM_3_18_FAMILY := true
-else ifeq ($(QCOM_HARDWARE_VARIANT),msm8998)
-    TARGET_USES_QCOM_UM_FAMILY := true
-    TARGET_USES_QCOM_UM_4_4_FAMILY := true
-else ifeq ($(QCOM_HARDWARE_VARIANT),sdm845)
-    TARGET_USES_QCOM_UM_FAMILY := true
-    TARGET_USES_QCOM_UM_4_9_FAMILY := true
-else ifeq ($(QCOM_HARDWARE_VARIANT),sm8150)
-    TARGET_USES_QCOM_UM_FAMILY := true
-    TARGET_USES_QCOM_UM_4_14_FAMILY := true
-endif
+include vendor/aosp/config/BoardConfigQcomUmPlatform.mk
 
 PRODUCT_SOONG_NAMESPACES += \
     hardware/qcom/audio-caf/$(QCOM_HARDWARE_VARIANT) \

config/BoardConfigQcomDisplayOverride.mk
@@ -1,0 +1,5 @@
+QCOM_HARDWARE_VARIANT := $(FORCE_QCOM_DISPLAY_HAL_VARIANT)
+
+include vendor/aosp/config/BoardConfigQcomUmPlatform.mk
+
+PRODUCT_SOONG_NAMESPACES += hardware/qcom/display-caf/$(QCOM_HARDWARE_VARIANT)

PearlOS/platform_vendor_pearl

config/BoardConfigQcom.mk
@@ -97,6 +97,8 @@ ifneq ($(OVERRIDE_QCOM_HARDWARE_VARIANT),)
     QCOM_HARDWARE_VARIANT := $(OVERRIDE_QCOM_HARDWARE_VARIANT)
 endif
 
+include vendor/pearl/config/BoardConfigQcomUmPlatform.mk
+
 PRODUCT_SOONG_NAMESPACES += \
     hardware/qcom/audio-caf/$(QCOM_HARDWARE_VARIANT) \
     hardware/qcom/display-caf/$(QCOM_HARDWARE_VARIANT) \

config/BoardConfigQcomDisplayOverride.mk
@@ -1,0 +1,5 @@
+QCOM_HARDWARE_VARIANT := $(FORCE_QCOM_DISPLAY_HAL_VARIANT)
+
+include vendor/pearl/config/BoardConfigQcomUmPlatform.mk
+
+PRODUCT_SOONG_NAMESPACES += hardware/qcom/display-caf/$(QCOM_HARDWARE_VARIANT)
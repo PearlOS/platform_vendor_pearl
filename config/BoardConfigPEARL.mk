70721861cfe79c04b16076f0b6d3cd00172e6ace
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-03 18:13:20 +0000

    vendor: Add special conditions for devices with prebuilt display hal
    * FORCE_QCOM_DISPLAY_HAL_VARIANT
    
    Change-Id: I0045b6b58059c8e39724059e35a40cf8e9fc0039

--- config/BoardConfigPEARL.mk
+++ config/BoardConfigPEARL.mk
@@ -1,6 +1,11 @@
-include vendor/du/config/BoardConfigKernel.mk
+include vendor/pearl/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/pearl/config/BoardConfigQcom.mk
 endif
+
+ifneq ($(FORCE_QCOM_DISPLAY_HAL_VARIANT),)
+include vendor/pearl/config/BoardConfigQcomDisplayOverride.mk
+endif
+
 include vendor/pearl/config/BoardConfigSoong.mk
